14e57cd953e0a71c58cf8d32b36be373d9c80735
Matus <matus.tibensky@naytrolabs.com>
2019-01-24 12:54:38 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Harry Potter',
-        text: 'wtf'
-    })
 });
 
 socket.on('disconnect', function () {
