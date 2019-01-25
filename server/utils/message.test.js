23be6a4fc045598e050d01480e3eaa9b2f47254b
Matus <matus.tibensky@naytrolabs.com>
2019-01-25 13:51:18 +0100

    Add geolocation support viac geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -10,5 +10,18 @@ describe('generateMessage', () => {
 
         expect(typeof message.createdAt).toBe('number');
         expect(message).toMatchObject({from,text});
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'Bejos';
+        var latitude = 48.235213;
+        var longitude = 17.0345543;
+        var url = 'https://www.google.com/maps?q=48.235213,17.0345543';
+        var locationMessage = generateLocationMessage(from, latitude, longitude);
+
+        expect(typeof locationMessage.createdAt).toBe('number');
+        expect(locationMessage).toMatchObject({from, url});
     });
 });
